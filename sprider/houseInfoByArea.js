483246ddd182edda5d069505004f37700b17cdff
wxrbwran <wxrbw@qq.com>
2018-08-02 16:05:18 +0800

    ????????????

--- sprider/houseInfoByArea.js
+++ sprider/houseInfoByArea.js
@@ -19,6 +19,7 @@ const getHouse = async (page = 1, area = '') => {
   let house = []
 
   $('.key-list .item-mod').each(function () {
+  	// console.log($(this).text())
     const name = $(this).find('.items-name').text()
     const address = $('.curr-area').text()
     const huxing = $(this).find('.huxing').text().trim().replace(/\s/g, '')
--- sprider/yigb.js
+++ sprider/yigb.js
@@ -1,38 +1,35 @@
 // 获取区域房源信息
 const cheerio = require('cheerio')
-const request = require('request-promise').defaults({
+const rp = require('request-promise').defaults({
   proxy: 'http://127.0.0.1:1080'
 })
 const fs = require('fs')
 const path = require('path')
-
-// console.log(sleep)
+const iconv = require("iconv-lite");
+var http=require('http');
 
 const getLists = async () => {
   const options = {
     uri: 'http://t66y.com/htm_data/7/1807/3225843.html',
     transform: body => {
-      console.log(body)
-      return cheerio.load(body)
+      // console.log(body)
+      const convertContent = iconv.decode(body, 'GBK')
+      console.log(convertContent)
+      return cheerio.load(convertContent)
     }
   }
   console.log(`正在爬${options.uri}`)
-  const $ = await request(options)
+  const $ = await rp(options)
   let urls = []
-
   const text = $('.tpc_content.do_not_catch').text()
   console.log(text)
-  // urls.push({
-  //   name,
-  //   huxing,
-  //   price,
-  //   aroundPrice,
-  //   address
-  // })
-  // fs.writeFileSync(path.resolve(__dirname, './data/json/urls.json'),
-  //   JSON.stringify(urls, null, 2), 'utf-8')
+   urls.push({
+     text,
+  })
+  fs.writeFileSync(path.resolve(__dirname, './data/json/urls.json'),
+     JSON.stringify(urls, null, 2))
 }
 
 module.exports = { getLists }
 
-getLists()
+getLists();
