0167284e1a649ba1b3b181d0352b8c27d1116276
wuxiaoran <wxrbw@qq.com>
2018-08-31 00:19:27 +0800

    fix

--- sprider/yigb.js
+++ sprider/yigb.js
@@ -4,33 +4,43 @@ const fs = require('fs')
 const path = require('path')
 
 const getLists = async () => {
-  const uri = 'http://t66y.com/htm_data/7/1807/3225843.html';
+  const uri = 'http://t66y.com/htm_data/7/1807/3225843.html'
   console.log(`正在爬${uri}`)
-  const price_crawler = new Crawler({
+  const priceCrawler = new Crawler({
     rateLimit: 1000,
     forceUTF8: true,
     incomingEncoding: 'gbk',
-    callback(err, res, done){
+    callback (err, res, done) {
       // console.log(err, res, done);
       if (err) {
-        console.log(err);
+        console.log(err)
       } else {
-        const $ = res.$;
-        let urls = [];
-        const text = $('.tpc_content.do_not_catch').text();
-        console.log(text);
-        console.log($('body').text());
-        urls.push({ text })
+        const $ = res.$
+        let urls = {}
+        const pageTitle = $('title').text().split(' - 技術討論區')[0]
+        const text = $('.tpc_content.do_not_catch').text()
+        const pairs = text.split(pageTitle)[1].split('.html')
+        urls.content = []
+        urls.title = pageTitle
+        for (const pair of pairs) {
+          const tmp = {}
+          const item = pair.split('https://')
+          tmp.title = item[0].split(/(下载|下載|【下载|【下載)/)[0].trim()
+          tmp.url = `${item[1]}.html`
+          urls.content.push(tmp)
+        }
+        console.log($('body').text())
+        urls.text = text
         fs.writeFileSync(path.resolve(__dirname, './data/json/urls.json'),
           JSON.stringify(urls, null, 2))
-        console.log(`爬取结束，text length: ${text.length}`)
+        console.log(`爬取结束，data length: ${urls.content.length}`)
       }
     }
   });
-  price_crawler.queue({
+  priceCrawler.queue({
     uri,
-    proxy: 'http://222.92.194.153:80'
-  });
+    proxy: 'http://118.244.236.27:1080'
+  })
 }
 
 module.exports = { getLists };
