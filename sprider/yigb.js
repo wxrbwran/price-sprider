c9155b75f08c22174c3b9f698e7b31bc8dd2493a
wuxiaoran <wxrbw@qq.com>
2018-08-31 19:32:26 +0800

    save

--- sprider/yigb.js
+++ sprider/yigb.js
@@ -78,39 +78,45 @@ http://t66y.com/htm_data/7/1805/3139610.html`;
 const getLists = async () => {
   const uris = lists.replace(/\s/g, '').split('http://').filter(uri => !!uri).map(i => `http://${i}`);
   console.log(uris);
-  const priceCrawler = new Crawler({
-    rateLimit: 5000,
-    forceUTF8: true,
-    incomingEncoding: 'gbk',
-    callback (err, res, done) {
-      // console.log(err, res, done);
-      if (err) {
-        console.log(err)
-      } else {
-        const $ = res.$
-        let urls = {}
-        const pageTitle = $('title').text().split(' - 技術討論區')[0]
-        const text = $('.tpc_content.do_not_catch').text()
-        const pairs = text.split(pageTitle)[1].split('.html')
-        urls.content = []
-        urls.title = pageTitle
-        for (const pair of pairs) {
-          const tmp = {}
-          const item = pair.split('https://')
-          if (!!item[1]) {
-            tmp.title = item[0].split(/(下载|下載|【下载|【下載)/)[0].trim()
-            tmp.url = `${item[1]}.html`
-            urls.content.push(tmp)
+  // const uri = 'http://t66y.com/htm_data/7/1807/3225843.html'
+
+  for (const uri of uris) {
+    const priceCrawler = new Crawler({
+      rateLimit: 5000,
+      forceUTF8: true,
+      incomingEncoding: 'gbk',
+      callback (err, res, done) {
+        // console.log(err, res, done);
+        if (err) {
+          console.log(err)
+        } else {
+          console.log(res);
+          fs.writeFileSync(path.resolve(__dirname, `./data/json/list`), JSON.stringify(res));
+          const $ = res.$
+          let urls = {}
+          const pageTitle = $('title').text().split(' - 技術討論區')[0]
+          const text = $('.tpc_content.do_not_catch').text()
+          const pairs = text.split(pageTitle)[1] ? text.split(pageTitle)[1].split('.html') : text.split('.html')
+          urls.content = []
+          urls.title = pageTitle
+          for (const pair of pairs) {
+            const tmp = {}
+            const item = pair.split('https://')
+            if (!!item[1]) {
+              tmp.title = item[0].split(/(下载|下載|【下载|【下載)/)[0].trim()
+              tmp.url = `${item[1]}.html`
+              urls.content.push(tmp)
+            }
           }
+          urls.text = text
+          fs.writeFileSync(path.resolve(__dirname, `./data/json/${pageTitle}.json`),
+            JSON.stringify(urls, null, 2))
+          console.log(`爬取结束，data length: ${urls.content.length}`)
         }
-        urls.text = text
-        fs.writeFileSync(path.resolve(__dirname, `./data/json/${pageTitle}.json`),
-          JSON.stringify(urls, null, 2))
-        console.log(`爬取结束，data length: ${urls.content.length}`)
       }
-    }
-  });
-  priceCrawler.queue(uris)
+    });
+    priceCrawler.queue(uri)
+  }
 }
 
 module.exports = { getLists };
